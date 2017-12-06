d50bf45861444385828c3d2e191be8245095ad2e
Linn <linn.martensson@hotmail.se>
2017-12-06 14:24:12 +0100

    added lightbox.js

--- Linn.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $("#displayCommentSection").click(function(){
-      $("#commentsSection").toggle();
-    })
-    $("#leaveComment").click(function() {
-      $(".yourComment").toggle();
-    });
-})
